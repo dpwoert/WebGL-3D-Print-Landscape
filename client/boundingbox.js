878a0560f34139d26b3cca978fbd6ec48e7b0a03
Hedva <jvanrheenen@gmail.com>
2013-12-14 16:11:26 +0100

    Update boundingbox.js

--- client/boundingbox.js
+++ client/boundingbox.js
@@ -9,21 +9,15 @@ function rad2deg (angle) {
 getBoundingBox = function(lat_degrees,lon_degrees,distance_in_km) {
 
     var array = [];
-    var radius = 6378; // of earth in km
+    var radius = 6378;
 
-    // bearings - FIX   
     var due_north = deg2rad(0);
     var due_south = deg2rad(180);
     var due_east = deg2rad(90);
     var due_west = deg2rad(270);
 
-    // convert latitude and longitude into radians 
     var lat_r = deg2rad(lat_degrees);
     var lon_r = deg2rad(lon_degrees);
-
-    // find the northmost, southmost, eastmost and westmost corners distance_in_km away
-    // original formula from
-    // http://www.movable-type.co.uk/scripts/latlong.html
 
     northmost  = Math.asin(Math.sin(lat_r)*Math.cos(distance_in_km/radius)+Math.cos(lat_r)*Math.sin(distance_in_km/radius)*Math.cos(due_north));
     southmost  = Math.asin(Math.sin(lat_r)*Math.cos(distance_in_km/radius)+Math.cos(lat_r)*Math.sin(distance_in_km/radius)*Math.cos(due_south));
@@ -37,7 +31,6 @@ getBoundingBox = function(lat_degrees,lon_degrees,distance_in_km) {
     eastmost = rad2deg(eastmost);
     westmost = rad2deg(westmost);
 
-    // sort the lat and long so that we can use them for a between query        
     if (northmost > southmost) { 
         lat1 = southmost;
         lat2 = northmost;
